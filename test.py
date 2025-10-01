7691517731d2dfcca27fbc83ec65cab785209ddf
alinac1888 <alinac44@nycstudents.net>
2025-10-01 09:33:16 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 game = int(input("Guess a number between 1-10"))
-while game != 8:
-    game = int(input("incorrect, Guess another number between 1-10"))
+while game < 8:
+    game = int(input("greater, guess another number between 1-10"))
+    
     if game == 8:
       print("correct")
